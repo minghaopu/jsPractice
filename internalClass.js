91e604957b687a3705fad493f9190015d5f36772
roneil <roneil_PMH@hotmail.com>
2017-01-31 00:01:40 -0500

    Internal Class Practice;

---
+++ internalClass.js
@@ -0,0 +1,23 @@
+(function() {
+	Object.prototype.toString.call([]);					// "[object Array]"
+	Object.prototype.toString.call([1, 2, 3]);			// "[object Array]"
+
+	Object.prototype.toString.call({});					// "[object Object]"
+	Object.prototype.toString.call({					// "[object Object]"
+		a: 1
+	});
+
+	Object.prototype.toString.call(/regex-literal/i);	// "[object RegExp]"
+
+	Object.prototype.toString.call(null);				// "[object Null]"
+
+	Object.prototype.toString.call(undefined);			// "[object Undefined]"
+
+	Object.prototype.toString.call("");					// "[object String]"
+	Object.prototype.toString.call("avd");				// "[object String]"
+
+	Object.prototype.toString.call(12);					// "[object Number]"
+	Object.prototype.toString.call(NaN);				// "[object Number]"
+
+	Object.prototype.toString.call(true);				// "[object Boolean]"
+})();
