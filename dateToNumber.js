7412a20a60a6006aacf0d996381b660fb44ebcbe
roneil <roneil_PMH@hotmail.com>
2017-02-06 23:21:39 -0500

    implicit coercion to string;

---
+++ dateToNumber.js
@@ -0,0 +1,3 @@
+(function() {
+	Date.now() === +new Date();  // true;
+})()
--- toStringAndJsonStringify.js
+++ toStringAndJsonStringify.js
@@ -145,12 +145,21 @@
 
 
 
+	[] + {};		// "[object Object]";
+	{} + [];		// 0
 
 
-
-
-
-
+	// + "" invoke valueOf, String() invoke toString
+	var a = {
+		valueOf: function() {
+			return 42;
+		},
+		toString: function() {
+			return 4;
+		}
+	}
+	a + "";			// "42"
+	String(a)		// "4"
 
 
 })();
