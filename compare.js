c1fedc73352e0c61a3883447a3c27538f1d1fa7b
roneil <roneil_PMH@hotmail.com>
2017-02-09 22:17:46 -0500

    falsy case;

--- compare.js
+++ compare.js
@@ -26,4 +26,57 @@
 	// if (a === 2 && a === 3) {
 	// 	console.log('this not happen');
 	// }
+
+	"0" == null;				// false;
+	"0" == undefined;			// false;
+	"0" == false;				// true;
+	"0" == NaN;					// false;
+	"0" == 0;					// true;
+	"0" == "";					// false;
+	"0" == [];					// false;
+	"0" == {};					// false;
+
+	false == null;				// false;
+	false == undefined;			// false;
+	false == NaN;				// false;
+	false == 0;					// true;
+	false == "";				// true;
+	false == [];				// true;
+	false == {};				// false;
+
+	"" == null;					// false;
+	"" == undefined;			// fasle;
+	"" == NaN;					// fasle;
+	"" == 0;					// true;
+	"" == [];					// true;
+	"" == {};					// false;
+
+	0 == null;					// fasle;
+	0 == undefined;				// false;
+	0 == NaN;					// fasle;
+	0 == [];					// true;
+	0 == {};					// fasle;
+
+	[] == ![];					// true;
+	2 == [2];					// true;
+	"" == [null];				// true;
+
+	0 == "\n";					// true;
+	0 == "\t";					// true;
+
+	var a = [42];
+	var b = ["43"];
+	a < b;						// true;
+	var c = ["044"];
+	b < c;						// false;
+
+
+	var a = {b : 42};
+	var b = {b : 43};
+	a < b;						// fasle
+	a > b;						// false
+	a == b;						// false
+	a >= b;						// true;
+	a <= b;						// true;
+
 })()
